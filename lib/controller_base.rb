ffed7d0d531e6708cd965b504e4b350053cc4d86
Live session user <gabrielvt6@gmail.com>
2018-03-14 16:28:08 -0700

    Complete steps II and III

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -2,7 +2,7 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
-
+require 'active_support/inflector'
 class ControllerBase
   attr_reader :req, :res, :params
 
@@ -14,21 +14,34 @@ class ControllerBase
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'Double render error!' if already_built_response?
+    res.header['location'] = url
+    res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
+  # Raise an error! if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'Double render error!' if already_built_response?
+    res.headers['Content-Type'] = content_type
+    res.body = [content]
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+    contents = File.read(path)
+    template = ERB.new(contents).result(binding)
+    render_content(template, 'text/html')
   end
 
   # method exposing a `Session` object
